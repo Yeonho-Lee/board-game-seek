4fa14368df663749bb7af0a542856132b6267f9b
Yeonho Lee <6464106@snu.ac.kr>
2024-10-14 12:48:41 +0900

    changed sql query

--- services/tests.ts
+++ services/tests.ts
@@ -1,22 +1,21 @@
 /* /services/tests.ts */
 
-'use server'
+"use server";
 
 import { sql } from "@vercel/postgres";
 
 export interface Posts {
-	id : number,
-	title : string,
-	content : string,
-	author : string,
+    id: number;
+    title: string;
+    content: string;
+    author: string;
 }
 
 export async function getPostsData(): Promise<Posts[]> {
-	try {
-		const { rows }: { rows: Posts[] } = await sql
-			`SELECT id, title, content, author FROM posts;`;
-		return rows;
-	} catch (error) {
-		throw new Error('Failed to fetch posts data');
-	}
+    try {
+        const { rows }: { rows: Posts[] } = await sql`SELECT id, title, content, author FROM tests;`;
+        return rows;
+    } catch (error) {
+        throw new Error("Failed to fetch posts data");
+    }
 }
