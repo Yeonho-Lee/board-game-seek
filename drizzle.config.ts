deb803d98c2415d82b1b59e7ad12c3baa4c76c62
Yeonho Lee <6464106@snu.ac.kr>
2024-10-18 08:29:25 +0900

    test: :white_check_mark: set up Drizzle with initial test run

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,23 +1,24 @@
-import { defineConfig } from 'drizzle-kit';
-import { loadEnvFile } from 'node:process';
-import { object, parse, string } from 'valibot';
+import { defineConfig } from "drizzle-kit";
+import { loadEnvFile } from "node:process";
+import { object, parse, string } from "valibot";
 
-loadEnvFile('.env.local');
+loadEnvFile(".env.local");
 
 const dbCredentials = parse(
-	object({
-		DB_NAME: string(),
-		DB_USERNAME: string(),
-		DB_PASSWORD: string(),
-	}),
-	process.env,
+    object({
+        DB_NAME: string(),
+        DB_USERNAME: string(),
+        DB_PASSWORD: string(),
+    }),
+    process.env
 );
 
 export default defineConfig({
-	dialect: 'postgresql',
-	schema: './drizzle/schema.ts',
-	dbCredentials: {
-		url: `postgres://${dbCredentials.DB_USERNAME}:${dbCredentials.DB_PASSWORD}@localhost:5432/${dbCredentials.DB_NAME}`,
-	},
-	casing: 'snake_case',
+    dialect: "postgresql",
+    schema: "./drizzle/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+        url: `postgres://${dbCredentials.DB_USERNAME}:${dbCredentials.DB_PASSWORD}@localhost:5432/${dbCredentials.DB_NAME}`,
+    },
+    casing: "snake_case",
 });
