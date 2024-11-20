64acb29d4790197fd6ad01afda70587848f49fb6
Yeonho Lee <6464106@snu.ac.kr>
2024-11-20 15:56:19 +0900

    style:

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import BoardgameRankings from "@/components/boardgame/boardgame-rankings";
 
 export default function BoardgameRankingsPage() {
-    return (
-        <div className="container mx-auto py-10">
-            <h1 className="text-3xl font-bold mb-6">Boardgame Rankings</h1>
-            <BoardgameRankings />
-        </div>
-    );
+	return (
+		<div className="container mx-auto py-10">
+			<h1 className="mb-6 text-3xl font-bold">Boardgame Rankings</h1>
+			<BoardgameRankings />
+		</div>
+	);
 }
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,20 +5,20 @@ import { object, parse, string } from "valibot";
 loadEnvFile(".env.local");
 
 const dbCredentials = parse(
-    object({
-        DB_NAME: string(),
-        DB_USERNAME: string(),
-        DB_PASSWORD: string(),
-    }),
-    process.env
+	object({
+		DB_NAME: string(),
+		DB_USERNAME: string(),
+		DB_PASSWORD: string(),
+	}),
+	process.env,
 );
 
 export default defineConfig({
-    dialect: "postgresql",
-    schema: "./drizzle/schema.ts",
-    out: "./drizzle",
-    dbCredentials: {
-        url: `postgres://${dbCredentials.DB_USERNAME}:${dbCredentials.DB_PASSWORD}@localhost:5432/${dbCredentials.DB_NAME}`,
-    },
-    casing: "snake_case",
+	dialect: "postgresql",
+	schema: "./drizzle/schema.ts",
+	out: "./drizzle",
+	dbCredentials: {
+		url: `postgres://${dbCredentials.DB_USERNAME}:${dbCredentials.DB_PASSWORD}@localhost:5432/${dbCredentials.DB_NAME}`,
+	},
+	casing: "snake_case",
 });
